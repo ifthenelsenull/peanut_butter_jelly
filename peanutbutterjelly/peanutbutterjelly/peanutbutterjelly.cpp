90722e1848998292d606b31c33de6e110fc9f579
ifthenelsenull <ifthenelsenull@gmail.com>
2015-12-06 17:30:09 -0600

    updating parameters

--- peanutbutterjelly/peanutbutterjelly/peanutbutterjelly.cpp
+++ peanutbutterjelly/peanutbutterjelly/peanutbutterjelly.cpp
@@ -4,17 +4,36 @@
 #include "stdafx.h"
 #include <iostream>
 #include <conio.h>
+#include <string>
 
 using namespace std;
 
 
+std::string parameter1;
+std::string parameter2;
 
 int main()
 {
+	std::cout<< "enter parameter 1:  ";
+	std::cin >> parameter1;
 
-	std::cout << "test pbj";
-    return 0;
+	std::cout << "enter parameter 2:  ";
+	std::cin >> parameter2;
+
+	//cout << "enter parameter 1:  ";
+	//cin >> parameter1;
+
+	//cout << "enter parameter 1:  ";
+	//cin >> parameter1;
+
+	std::cout << parameter1;
+	_getch();
+	std::cout << parameter2;
 
 	_getch();
+
+    return 0;
+
 }
+
 
